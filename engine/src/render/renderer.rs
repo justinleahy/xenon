5305fff985c303ed22b7e5b6ced84e04853e2cf1
Justin Leahy <justinjleahy@gmail.com>
2026-05-17 09:53:58 -0400

    Changed triangle pipeline to quad and added index buffer

--- engine/src/render/renderer.rs
+++ engine/src/render/renderer.rs
@@ -1,4 +1,47 @@
 use super::RenderError;
+use bytemuck::{Pod, Zeroable};
+use wgpu::util::DeviceExt;
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, Pod, Zeroable)]
+struct Vertex {
+    position: [f32; 2],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    const ATTRIBUTES: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x2, 1 => Float32x3];
+
+    fn layout() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTES,
+        }
+    }
+}
+
+const QUAD_VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.25, 0.25],
+        color: [0.95, 0.9, 0.55],
+    },
+    Vertex {
+        position: [-0.25, -0.25],
+        color: [0.35, 0.7, 1.0],
+    },
+    Vertex {
+        position: [0.25, -0.25],
+        color: [0.9, 0.35, 0.45],
+    },
+    Vertex {
+        position: [0.25, 0.25],
+        color: [0.45, 0.95, 0.65],
+    },
+];
+
+const QUAD_INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
 
 pub struct Renderer<'window> {
     _instance: wgpu::Instance,
@@ -7,7 +50,10 @@ pub struct Renderer<'window> {
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    triangle_pipeline: wgpu::RenderPipeline,
+    quad_pipeline: wgpu::RenderPipeline,
+    quad_vertex_buffer: wgpu::Buffer,
+    quad_index_buffer: wgpu::Buffer,
+    quad_index_count: u32,
 }
 
 impl<'window> Renderer<'window> {
@@ -48,7 +94,7 @@ impl<'window> Renderer<'window> {
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Xenon Triangle Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/triangle.wgsl").into()),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/colored.wgsl").into()),
         });
 
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
@@ -57,13 +103,13 @@ impl<'window> Renderer<'window> {
             immediate_size: 0,
         });
 
-        let triangle_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Xenon Triangle Pipeline"),
+        let quad_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Xenon Quad Pipeline"),
             layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
-                buffers: &[],
+                buffers: &[Vertex::layout()],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
             fragment: Some(wgpu::FragmentState {
@@ -83,6 +129,20 @@ impl<'window> Renderer<'window> {
             cache: None,
         });
 
+        let quad_vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Xenon Quad Vertex Buffer"),
+            contents: bytemuck::cast_slice(QUAD_VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let quad_index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Xenon Quad Index Buffer"),
+            contents: bytemuck::cast_slice(QUAD_INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let quad_index_count = QUAD_INDICES.len() as u32;
+
         Ok(Self {
             _instance: instance,
             surface,
@@ -90,7 +150,10 @@ impl<'window> Renderer<'window> {
             device,
             queue,
             config,
-            triangle_pipeline,
+            quad_pipeline,
+            quad_vertex_buffer,
+            quad_index_buffer,
+            quad_index_count,
         })
     }
 
@@ -155,8 +218,11 @@ impl<'window> Renderer<'window> {
                 multiview_mask: None,
             });
 
-            render_pass.set_pipeline(&self.triangle_pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.quad_pipeline);
+            render_pass.set_vertex_buffer(0, self.quad_vertex_buffer.slice(..));
+            render_pass
+                .set_index_buffer(self.quad_index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.quad_index_count, 0, 0..1);
         }
 
         self.queue.submit([encoder.finish()]);
